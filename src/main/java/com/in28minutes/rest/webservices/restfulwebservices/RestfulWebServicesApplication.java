0323b2f657b86c3dd2729862c334186a047cd7be
zcmich <zafackmichel@yahoo.fr>
2023-04-22 17:40:16 +0100

    intial commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
-//A bean is an object that is instantiated, assembled, and otherwise managed by a Spring IoC container.
+//A bean is an object that is instantiated, assembled, and otherwise managed by a Spring IoC container.d
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServicesApplication.class, args);
     }
